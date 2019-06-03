35a1a9b8fa2db10125afa53cabda1df8a9daa546
zachary <954979487@qq.com>
2019-06-03 17:37:03 +0800

    删除日志配置，暂时不关注，以yml为主

--- eureka-server/src/main/java/com/gzw/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/gzw/EurekaServerApplication.java
@@ -1,13 +1,13 @@
 package com.gzw;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
